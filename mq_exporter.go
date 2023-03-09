273354a56c31a7fde41e4ab6006ffb0c486b9fe3
Andreas Gebhardt <agebhar1@googlemail.com>
2023-03-09 14:04:30 +0100

    update »github.com/prometheus/{common,exporter-toolkit}«

--- mq_exporter.go
+++ mq_exporter.go
@@ -24,6 +24,7 @@ import (
 
 	"github.com/agebhar1/mq_exporter/collector"
 	"github.com/agebhar1/mq_exporter/mq"
+	"github.com/alecthomas/kingpin/v2"
 	"github.com/go-kit/log"
 	"github.com/go-kit/log/level"
 	"github.com/prometheus/client_golang/prometheus"
@@ -34,7 +35,6 @@ import (
 	"github.com/prometheus/common/version"
 	"github.com/prometheus/exporter-toolkit/web"
 	webflag "github.com/prometheus/exporter-toolkit/web/kingpinflag"
-	kingpin "gopkg.in/alecthomas/kingpin.v2"
 )
 
 var name = "mq_exporter"
